decf4180a2de627f43f92b124d9a7fb8dc7747eb
Peiyang Wang <plukewang@gmail.com>
2024-04-15 15:35:40 -0500

    spiffed up loading and error pages

--- src/components/Routes/Error.jsx
+++ src/components/Routes/Error.jsx
@@ -1,18 +1,27 @@
-import { useRouteError } from "react-router-dom"
+import { Link, useRouteError } from "react-router-dom"
 import styles from "../../styles/Page.module.css";
+import linkStyle from "../../styles/Link.module.css"
 import Cover from "../Cover";
+import { useRef } from "react";
 
 export default function Error(){
     let error = useRouteError();
+    let imageRef = useRef(null)
     console.log(error)
+
     return (
         
         <div className={styles.background}>
-        \<Cover />
+        <Cover />
             <h1>Oh shoot!</h1>
             <h2>You aren't supposed to be here!</h2>
-            <p>{error.status+" "+error.statusText}</p>
-            
+
+            <img ref={imageRef} src="../../error.png" alt = 'error image' height={200}/>
+            <h1>Error {error.status}</h1>
+
+            <p>{error.data}</p>        
+
+            <Link to={'/'}><button className={linkStyle.link}><p>Take me back home</p></button></Link>    
         </div>
     )
 }
--- src/components/Routes/Loading.jsx
+++ src/components/Routes/Loading.jsx
@@ -1,8 +1,18 @@
+import gsap from "gsap"
 
 export default function Loading(){
+    gsap.to("#image", {
+        rotation: 360,
+        duration: 2,
+        repeat: -1, // -1 for infinite loop
+        ease: "linear"
+      });
+    
     return (
-        <div style={{backgroundColor: "#161315"}}>
+        <div style={{backgroundColor: "#161315", display: "flex", flexDirection: "column", alignItems: "center"}}>
             <h1 style={{margin: 0, padding: "100px"}}>Loading...</h1>
+            <p style={{textAlign: "center"}}>Spinning up the data. This may take a few seconds...</p>
+            <img id = 'image' src="../../favicon.png" alt="loading"/>
         </div>
     )
 
